fcf6ca7565232b246c3478212ec8e91c1addbdb8
Frank Violette <frank@redpepperpixels.com>
2016-07-03 22:51:25 -0300

    Initialized login page

---
+++ mAPT/MyPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace mAPT
+{
+	public partial class MyPage : ContentPage
+	{
+		public MyPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
---
+++ mAPT/loginPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace mAPT
+{
+	public partial class loginPage : ContentPage
+	{
+		public loginPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
